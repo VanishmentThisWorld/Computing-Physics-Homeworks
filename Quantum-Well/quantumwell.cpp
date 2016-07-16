aae9db76788f9fa4a76064b05b1a209c6895a1cf
VanishmentThisWorld <zhouyj12@fudan.edu.cn>
2016-07-16 23:33:13 +0800

    Update quantumwell.cpp

--- Quantum-Well/quantumwell.cpp
+++ Quantum-Well/quantumwell.cpp
@@ -25,32 +25,32 @@ int _tmain(int argc, _TCHAR* argv[])
 	z     eigenvectors       DOUBLE[N*m]
 	======================================
 	*/
-	char jobz = 'V';							// 'N' eigval only; 'V' both eigval and eigfun
-	char range = 'I';							// 'A' all eigs; 'V' vl < eig <= vu; 'I' #il - #iu eigs 
+	char jobz = 'V';		            // 'N' eigval only; 'V' both eigval and eigfun
+	char range = 'I';			    // 'A' all eigs; 'V' vl < eig <= vu; 'I' #il - #iu eigs 
 	
 	// define Hamiltonian
-	double *d = new double[N];					// diagnol elements
-	double *e = new double[N - 1];				// off-diagnol elements
+	double *d = new double[N];		    // diagnol elements
+	double *e = new double[N - 1];		    // off-diagnol elements
 	for (int i = 0; i < N; i++)
 		d[i] = 1 / dx / dx + potential(xmin + dx * i);
 	for (int i = 0; i < N - 1; i++)
 		e[i] = -0.5 / dx / dx;
 	// define Hamiltonian
 
-	double vl = -5,								// min eig,    range = 'V'
-		vu = 5;								    // max eig,    range = 'V'
-	int il = 1,								    // min # eig,  range = 'I'
-		iu = NumOfEigs,			    			// max # eig,  range = 'I'
-		m,									    // OUTPUT: # of eigs found
+	double vl = -5,				    // min eig,    range = 'V'
+		vu = 5;				    // max eig,    range = 'V'
+	int il = 1,				    // min # eig,  range = 'I'
+		iu = NumOfEigs,			    // max # eig,  range = 'I'
+		m,				    // OUTPUT: # of eigs found
 	    ldz = N,                                // Leading Dimension of z (length of each eigfun) >=N
-		nzc = NumOfEigs;                        // Number of z Counts (more than found eigs) >=m
+		nzc = NumOfEigs;                    // Number of z Counts (more than found eigs) >=m
 	double *w = new double[N];                  // OUTPUT: all eigvals
 	double *z = new double[N * NumOfEigs];      // OUTPUT: eigfuns, z(1:ldz,1:nzc): [N * m]
 	int *isuppz = new int[NumOfEigs * 2];       // nonzero elements in z, >= m*2, not of much use
 	int tryrac = 1,								// 0 free accuracy, 1 high accuracy
-		lwork = N * 18,                         // size of workspace work
-		liwork = N * 10,                        // size of workspace iwork
-		info;                                   // 0 SUCCESS, else something wrong
+		lwork = N * 18,                     // size of workspace work
+		liwork = N * 10,                    // size of workspace iwork
+		info;                               // 0 SUCCESS, else something wrong
 	double *work = new double[lwork];           // workspace? larger than 18*N
 	int *iwork = new int[liwork];               // workspace? larger than 10*N
 	
